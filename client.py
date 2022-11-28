34308c49a9f0b0d0ac03a241b25d42408027253f
ayatisrivastava <62139807+ayatisrivastava@users.noreply.github.com>
2022-11-28 17:01:35 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+from threading import Thread
+nickname = input("Chosse your name")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip_address = '192.168.0.158'
+port = 8000
+client.connect((ip_address, port))
+print("Connected with server.")
+def receive():
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('UTF-8'))
+            else:
+                print(message)
+        except:
+            print("An error occured")
+            client.close()
+            break
+def write():
+    while True:
+        message = '{}:{}'.format(nickname,input(''))
+        client.send(message.encode('utf-8'))
+receive_thread = Thread(target = receive)
+receive_thread.start()
+write_thread = Thread(target = write)
+write_target.start()
